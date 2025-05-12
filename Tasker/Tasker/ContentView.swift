083619fe7c67d9a7c610667c0c4d22c275678477
paris-phan <37759794+paris-phan@users.noreply.github.com>
2025-05-12 04:05:55 -0400

    Xcode project setup + gitignore DS_Store

---
+++ Tasker/Tasker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tasker
+//
+//  Created by Paris Phan on 5/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("boilerplate code page")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tasker/Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Paris Phan on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
